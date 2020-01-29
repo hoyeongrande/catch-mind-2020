491b185fc7d6004629cf8eaadef05a8ddf7595f0
hooy21c@gmail.com <hooy21c@gmail.com>
2020-01-29 23:50:43 +0900

    Apply .gitignore

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import { join } from "path";
+import express from "express";
+import socketIO from "socket.io";
+import logger from "morgan";
+
+const PORT = 5001;
+const app = express();
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
+app.use(express.static(join(__dirname, "static")));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListening = () =>
+  console.log(`✅ Server running: http://localhost:${PORT}`);
+
+const server = app.listen(PORT, handleListening);
+
+const io = socketIO.listen(server);
+
+let sockets = [];
+
+io.on("connection", socket => {
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotification", {
+      message,
+      nickname: socket.nickname || "Banana"
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
+});
