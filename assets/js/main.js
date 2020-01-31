e819f877546e3ba0fdf3d5b9da38516ef69f9d96
hooy21c@gmail.com <hooy21c@gmail.com>
2020-01-31 23:33:33 +0900

    20-01-31

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-const body = document.querySelector("body");
-const nickname = localStorage.getItem("nickname");
-
-const LOGGED_OUT = "loggedOut";
-const LOGGED_IN = "loggedIn";
-
-if (nickname === null) {
-  body.className = LOGGED_OUT;
-} else {
-  body.className = LOGGED_IN;
-}
+import "./sockets";
+import "./login";
+import "./notifications";
