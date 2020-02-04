0ed6f605f2830f45cac795337b31bd6d0b1b2a5b
hooy21c@gmail.com <hooy21c@gmail.com>
2020-02-04 17:27:53 +0900

    20-02-04
    chatting complete

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -1,11 +1,12 @@
-const notifications = document.getElementById("jsNotifications");
+const body = document.querySelector("body");
 
 const fireNotification = (text, color) => {
   const notification = document.createElement("div");
   notification.innerText = text;
-  notifications.appendChild(notification);
+  notification.style.backgroundColor = color;
+  notification.className = "notification";
+  body.appendChild(notification);
 };
-
 export const handleNewUser = ({ nickname }) => {
   fireNotification(`${nickname} just joined !`, "rgb(0, 122, 255)");
 };
